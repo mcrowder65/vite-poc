e2eff5f58915bc1db53000ffb5751d91367f4549
Matt Crowder <mcrowder65@gmail.com>
2022-07-09 13:01:19 -0500

    run against mine

--- screenshots/example.spec.js
+++ screenshots/example.spec.js
@@ -1,6 +1,12 @@
 const { test, expect } = require("@playwright/test")
 
 test("example test", async ({ page }) => {
-  await page.goto("https://playwright.dev")
+  await page.goto("localhost:3000")
+  await expect(page).toHaveScreenshot()
+})
+
+test("numbers", async ({ page }) => {
+  await page.goto("localhost:3000")
+  await page.locator("data-testid=my-button").click()
   await expect(page).toHaveScreenshot()
 })
